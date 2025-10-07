6bae02d9f7a84a7b8cd985717393f6e839d62f87
jackrao-lyra <jack@lyratechnologies.com.au>
2025-10-07 12:24:03 +1100

    fix getAllNamespaces infinite loop bug

--- src/server/api/routers/pinecone.ts
+++ src/server/api/routers/pinecone.ts
@@ -30,7 +30,10 @@ export async function getAllNamespaces() {
       break;
     }
 
-    currentPage = await pineconeIndex.listNamespaces();
+    currentPage = await pineconeIndex.listNamespaces(
+      100,
+      currentPage.pagination?.next,
+    );
   }
 
   return allNamespaces;
